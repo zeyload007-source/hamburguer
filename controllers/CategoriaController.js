dd791542c0fd3396884158a231973d8deb848218
Anderson Felipe <zeyload007@gmail.com>
2026-04-18 18:02:28 -0300

    Update CategoriaController.js

--- controllers/CategoriaController.js
+++ controllers/CategoriaController.js
@@ -1,6 +1,7 @@
 import Categoria from "../models/Categoria.js";
+import Produto from "../models/Produto.js";
 
- const CategoriaController = {
+const CategoriaController = {
   create: async (req, res) => {
     try {
       const categoria = await Categoria.create(req.body);
@@ -8,69 +9,81 @@ import Categoria from "../models/Categoria.js";
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-   },
+  },
+
   findAll: async (req, res) => { 
-    try
-    {
-      const categorias = await Categoria.findAll();
+    try {
+      const categorias = await Categoria.findAll({
+        include: [
+          {
+            model: Produto,
+            as: 'produtos'
+          }
+        ]
+      });
+
       if (categorias.length === 0) {
         throw new Error('Nenhuma categoria encontrada');
       }
+
       res.status(200).json(categorias);
-    }catch (error) {
+    } catch (error) {
       res.status(500).json({ error: error.message });
     }
-   },
+  },
 
   findById: async (req, res) => { 
     try{
-        const categoria = await Categoria.findByPk(req.params.id);
-        if (categoria) {
-          res.status(200).json(categoria);
-        } else {
-          res.status(404).json({ error: 'Categoria nao encontrada' });
-        }
+      const categoria = await Categoria.findByPk(req.params.id);
+      if (categoria) {
+        res.status(200).json(categoria);
+      } else {
+        res.status(404).json({ error: 'Categoria nao encontrada' });
+      }
     }catch(error){
       res.status(500).json({ error: error.message });
     }
-   },
+  },
+
   update: async (req, res) => {
     try{
-        const categoria = await Categoria.findByPk(req.params.id);
-        if (categoria) {
-          await categoria.update(req.body);
-          res.status(200).json(categoria);
-        } else {
-          res.status(404).json({ error: 'Categoria nao encontrada' });
-        }
+      const categoria = await Categoria.findByPk(req.params.id);
+      if (categoria) {
+        await categoria.update(req.body);
+        res.status(200).json(categoria);
+      } else {
+        res.status(404).json({ error: 'Categoria nao encontrada' });
+      }
     }catch(error){
       res.status(500).json({ error: error.message });
     }
   },
+
   delete: async (req, res) => { 
     try{
-        const categoria = await Categoria.findByPk(req.params.id);
-        if (categoria) {
-          await categoria.destroy();
-          res.status(200).json({ message: 'Categoria excluida com sucesso' });
-        } else {
-          res.status(404).json({ error: 'Categoria nao encontrada' });
-        }
+      const categoria = await Categoria.findByPk(req.params.id);
+      if (categoria) {
+        await categoria.destroy();
+        res.status(200).json({ message: 'Categoria excluida com sucesso' });
+      } else {
+        res.status(404).json({ error: 'Categoria nao encontrada' });
+      }
     }catch(error){
       res.status(500).json({ error: error.message });
     }
   },
+
   restaure : async (req,res) =>{
     try{
-        const categoria = await Categoria.findByPk(req.params.id, {paranoid : false});
-        if (categoria){
-            await categoria.restore();
-            res.status(200).json({ message: 'Categoria restaurada com sucesso' });
-        }else{
-            res.status(404).json({ error: 'Categoria nao encontrada' });
-        }
+      const categoria = await Categoria.findByPk(req.params.id, {paranoid : false});
+      if (categoria){
+        await categoria.restore();
+        res.status(200).json({ message: 'Categoria restaurada com sucesso' });
+      }else{
+        res.status(404).json({ error: 'Categoria nao encontrada' });
+      }
     }catch(error){
-        res.status(500).json({ error: error.message });
+      res.status(500).json({ error: error.message });
     }
   }
 };
